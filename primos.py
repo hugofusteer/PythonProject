7fe7ac6e62815645447ad39f4fd2a8bfc6c4a75b
hugo <martinfusterhugo@gmail.com>
2023-05-04 08:37:19 +0200

    Scripts

--- primos.py
+++ primos.py
@@ -1,2 +1,8 @@
-num1 = int(input("Intodueix un nombre"))
-x = 3
+nombre = int(input("Intodueix un nombre"))
+
+for i in range (2,nombre):
+        print ("Es divident", int(nombre), "/", int(i), "que dona: ", (nombre/i))
+        if (nombre / i == 0):
+                print ("No és primer")
+                exit (0)
+print ("És un nombre primer")
